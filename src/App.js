121a79d0c04f861d102abfe932d06d1aee401303
Peter-Glo <thomasayomide966@gmail.com>
2021-01-23 13:30:45 +0100

    Newly created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+	sta
+	render() {
+   return (
+		<React.Fragment>
+			<div className="container text-center">
+				<h1>Hello React App!</h1>
+			</div>
+		</React.Fragment>
+   )
+  }
+}
+
+export default App;
